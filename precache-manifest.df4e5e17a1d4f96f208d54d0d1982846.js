eeea085f825b5f94871e10d6d254e92bc61a20bf
birneh <birnesh1996@gmail.com>
2020-01-15 21:01:05 +0900

    Updates

---
+++ precache-manifest.df4e5e17a1d4f96f208d54d0d1982846.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3811dafdcda177ea01c21b12ee812157",
+    "url": "/my-blog/index.html"
+  },
+  {
+    "revision": "12e7e9a16dcb84f11b56",
+    "url": "/my-blog/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0ff72f24cdfef5072e82",
+    "url": "/my-blog/static/js/2.e7224542.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-blog/static/js/2.e7224542.chunk.js.LICENSE"
+  },
+  {
+    "revision": "12e7e9a16dcb84f11b56",
+    "url": "/my-blog/static/js/main.a9fba68e.chunk.js"
+  },
+  {
+    "revision": "971b58f4fb4fd45ecd79",
+    "url": "/my-blog/static/js/runtime-main.cf125f2f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-blog/static/media/logo.5d5d9eef.svg"
+  }
+]);
